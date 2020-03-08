3830363b3c5e78be1b121e2c53bcb618673b1e6d
webdevAlina@gmail.com <hpcproyalplay19>
2020-03-08 18:05:19 +0200

    add minor exception handling

--- ipl/_logging.py
+++ ipl/_logging.py
@@ -9,11 +9,6 @@ def configure_logger(is_debug: bool,
     format_ = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     date_format = '%Y-%m-%d %H:%M:%S'
     formatter = logging.Formatter(format_, date_format)
-    if logs_file:
-        file_handler = logging.FileHandler(logs_file, 'w')
-        file_handler.setFormatter(formatter)
-        file_handler.setLevel(logging.DEBUG)
-        logger.addHandler(file_handler)
     console_handler = logging.StreamHandler()
     logging_level = logging.DEBUG if is_debug else logging.INFO
     console_handler.setLevel(logging_level)
@@ -21,3 +16,11 @@ def configure_logger(is_debug: bool,
     logger.addHandler(console_handler)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
+    if logs_file:
+        try:
+            file_handler = logging.FileHandler(logs_file, 'w')
+            file_handler.setFormatter(formatter)
+            file_handler.setLevel(logging.DEBUG)
+            logger.addHandler(file_handler)
+        except Exception as error:
+            logger.warning(f'Unable to create log file, reason : "{error}"')
